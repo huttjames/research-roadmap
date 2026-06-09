ae868a879d9e211f9bb999b2b3ac826751254a04
James H <james.w.hutt@gmail.com>
2026-06-09 10:40:23 +0100

    Create preview artifact directory

--- scripts/start-preview.cmd
+++ scripts/start-preview.cmd
@@ -1,3 +1,4 @@
 @echo off
 cd /d "%~dp0.."
+if not exist "artifacts" mkdir "artifacts"
 start "" "C:\Program Files\nodejs\node.exe" "scripts\static-preview.mjs" > "artifacts\preview-out.log" 2> "artifacts\preview-err.log"
